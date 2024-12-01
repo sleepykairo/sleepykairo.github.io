440b2a1bfed93a33d2da4da8c111787b3a5d1534
sleepykairo <153457419+sleepykairo@users.noreply.github.com>
2024-12-01 17:23:53 -0600

    firebase FINALLY WORKING THANK THE HEAVENS

--- games/song_sleuth/app.js
+++ games/song_sleuth/app.js
@@ -1,7 +1,26 @@
 let playerId;
 let playerRef;
+let playerElements = {};
 
+function init() {
+    const allPlayersRef = firebase.default.database().ref("players");
 
+    allPlayersRef.on("value", (snapshot) => {
+        console.log("what does this even do");
+    })
+    allPlayersRef.on("child_added", (snapshot) => {
+        const addedPlayer = snapshot.val();
+
+        const playerElement = document.createElement("button");
+        const textNode = document.createTextNode(addedPlayer.name);
+        playerElement.appendChild(textNode);
+        playerElement.setAttribute("id", `button_${addedPlayer.id}`);
+        document.body.appendChild(playerElement);
+
+        playerElements[addedPlayer.id] = playerElement;
+        console.log("added " + addedPlayer.id);
+    });
+}
 
 firebase.default.auth().onAuthStateChanged((user) => {
     console.log(user);
@@ -14,10 +33,28 @@ firebase.default.auth().onAuthStateChanged((user) => {
             name: "H3LL0",
             id: playerId,
         })
-        playerRef.onDisconnect().remove();
+        playerRef.onDisconnect();
+
+        init();
     } else {
         //failed to log in probably
     }
-})
+});
 
 firebase.default.auth().signInAnonymously().catch(error => console.log(error.code, error.message));
+
+var usernameInput = document.getElementById("usernameInput");
+usernameInput.addEventListener("change", () => {
+    playerRef.set({name: usernameInput.value});
+    playerElements[playerId].textContent = usernameInput.value;
+
+/*    const para = document.createElement("p");
+    const textNode = document.createTextNode(usernameInput.value);
+    para.appendChild(textNode);
+    document.body.appendChild(para);*/
+});
+
+var logAllPlayersButton = document.getElementById("logAllPlayers");
+logAllPlayersButton.addEventListener("click", () => {
+    console.log(playerElements);
+});
